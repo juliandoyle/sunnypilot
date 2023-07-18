4cab07e8b6b8f2f9acca9d49bea6f40006f595c4
juliandoyle <97332462+juliandoyle@users.noreply.github.com>
2023-07-18 16:50:19 +1000

    remove mock

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -184,7 +184,7 @@ def get_car(logcan, sendcan, experimental_long_allowed, num_pandas=1):
 
   if candidate is None:
     cloudlog.event("car doesn't match any fingerprints", fingerprints=fingerprints, error=True)
-    candidate = "mock"
+    candidate = "VW PASSAT NMS"
 
   CarInterface, CarController, CarState = interfaces[candidate]
   CP = CarInterface.get_params(candidate, fingerprints, car_fw, experimental_long_allowed, docs=False)
